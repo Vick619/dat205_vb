277c21a5cebb07473b8954e15385b780c1d04901
(s) Viktor Brelsford <vfbrelsford@pm7-0081.uopnet.plymouth.ac.uk>
2015-02-26 11:18:01 +0000

    Done some more chatbot

--- Chatbot/Chatbot.py
+++ Chatbot/Chatbot.py
@@ -1,35 +1,45 @@
-# Knights to was "ni" video reference
-# https://www.youtube.com/watch?v=zIV4poUZAQo
 
+learnWords = {}
 file = open("stop-words.txt")
 stopwords = file.readlines()
-#function to filter out stop words: and, if, where...
-def filterOutStopwords(filteringString):
+#####################################################################################
+def filterOutStopwords(filteringString): #function to filter out stop words: and, if, where...
     #filteredstring = ""
     for word in stopwords:
-        next = word.strip()
+        next = word.lower().strip()
         #print(next)
-        filteringString = filteringString.replace(" " + next + " ", " ")
+        filteringString = filteringString.replace(" " + next + " ", " ").lower()
     return filteringString
-#meeting the chat bot
-input = raw_input("Hi!, What is your name?: ")
+#####################################################################################
+def feelings(): #Feelings function, analyses what your mood is.
+    input = raw_input("How are you feeling?: ")
+    filtered = filterOutStopwords(input)
+    #if statement to see what the user is feeling.
+    if filtered == 'sad':
+        print("Im sorry to hear that.")
+    elif filtered == 'happy':
+        print("Im glad to hear! :)")
+    elif filtered == 'bored':
+        print("Thats why im here.")
+    elif len(filtered) == 0:
+        print("Come on, you can talk to me :)")
+        feelings()
+    else:
+        print("I dont know that emotion")
+        feelings()
+#####################################################################################
+def chatbotMemory():
+    input = raw_input("Why do you feel this way? Explain with its name then Description: ")#chatbot asks why you feel this way, stores info in a dictionary.
+    for words in input:
+        mem = input.split().lower()
+        
+#####################################################################################
+input = raw_input("Hi!, What is your name?: ")#meeting the chat bot
 filtered = filterOutStopwords(input)
 filtered = filtered.replace(" name " , " ")
 filtered = filtered.replace(" called " , " ")
-filtered = filtered.replace("my " , " ")
-print("Nice to meet you " + filtered)
-#chatbot asks how your feeling
-input = raw_input("How are you feeling?: ")
-filtered = filterOutStopwords(input)
-#if statement to see what the user is feeling.
-if filtered == 'sad':
-    print("Im sorry to hear that.")
-elif filtered == 'happy':
-    print("Im glad to hear! :)")
-elif filtered == 'bored':
-    print("Thats why im here.")
-elif len(filtered) == 0:
-    print("Please type something")
-else:
-    print("I dont know that emotion")
-#making section where chatbot asks why you feel this way.
+name = filtered.replace("my " , " ")
+print("Nice to meet you " + name)
+feelings() #chatbot asks how your feeling
+#####################################################################################
+#chatbotMemory()
