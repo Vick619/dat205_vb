08e321197faad636479b3b65a91cfa4b1f8e02ac
(s) Viktor Brelsford <vfbrelsford@pm7-0081.uopnet.plymouth.ac.uk>
2015-03-05 09:45:15 +0000

    Finished visualization

--- Pythonvideo/Videohack.py
+++ Pythonvideo/Videohack.py
@@ -2,17 +2,43 @@ from graphics import *
 import random
 import math
 
+colour = 0
+width = 0
+move= 0
+moveback = 600
+bordercolour = 255
+bordercolour1 = 0
 datafile = open("data.txt", 'r')
-data = math.floor(1).datafile
+data = datafile.readlines()
 window = GraphWin("Visualisation", 600, 600)
 
 background = Rectangle(Point(0, 0), Point(601, 601))
 background.setFill(color_rgb(255, 255, 255))
 background.draw(window)
 
-for draw in data:
-    ball = Circle(Point(data, data), 60)
-    ball.setFill(color_rgb(100, 100, 100))
+for stringvalue in data:
+    colour+=7
+    width+=0.1
+    move+=18
+    moveback-=18
+    bordercolour-=7
+    bordercolour1+=7
+    time.sleep(1)
+    intvalue = math.floor(float(stringvalue))
+    ball = Circle(Point(moveback, 300), intvalue)
+    ball.setOutline(color_rgb(bordercolour, bordercolour, bordercolour))
+    ball.setWidth(width)
+    ball.setFill(color_rgb(colour, colour, colour))
+    ball.draw(window)
+    ball = Circle(Point(move, 150), intvalue)
+    ball.setOutline(color_rgb(bordercolour1, bordercolour1, bordercolour1))
+    ball.setWidth(width)
+    ball.setFill(color_rgb(colour, colour, colour))
+    ball.draw(window)
+    ball = Circle(Point(move, 450), intvalue)
+    ball.setOutline(color_rgb(bordercolour1, bordercolour1, bordercolour1))
+    ball.setWidth(width)
+    ball.setFill(color_rgb(colour, colour, colour))
     ball.draw(window)
 
 window.getMouse()
