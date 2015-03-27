39c309def0dd3419f91b67a14f2088fe1d7f51e2
(s) Viktor Brelsford <vfbrelsford@pm7-0081.uopnet.plymouth.ac.uk>
2015-03-27 17:50:44 +0000

    finished chat bot and lights

--- ArduinoLEDs/pythonleds.py
+++ ArduinoLEDs/pythonleds.py
@@ -1,29 +1,49 @@
 import serial
+import mosquitto
 #establish serial port connection to arduino.
 port = serial.Serial("/dev/cu.usbmodem1411",9600, timeout=2)
 #run serial port script in pyserial-2.7/setup.py using terminal with :python setup.py install --user
-#######################################################################
 
-while True:
-	input = raw_input("On or off ? ") #Input stored in input
+client = mosquitto.Mosquitto("vicktor")
+client.connect("141.163.83.22")
+client.subscribe("lights")
 
-	if input == "on":    #If the input == "on","ON",'1' will turn LED ON.
+def messageReceived(broker, obj, msg):
+	global client
+	print("Message " + msg.topic + " containing: " + msg.payload)
+	#client = None
+	onoff = msg.payload
+	if(onoff == "ON"):
 		port.write("1")
-		print("LED ON.")
-	elif input == "ON":
-		port.write("1")
-		print("LED ON.")
-	elif input == "1":
-		port.write("1")
-		print("LED ON.")
-	elif input == "off":#If the input == "off","OFF",'0' will turn LED OFF.
+	elif(onoff == "OFF"):
 		port.write("0")
-		print("LED OFF.")
-	elif input == "OFF":
-		port.write("0")
-		print("LED OFF.")
-	elif input == "0":
-		port.write("0")
-		print("LED OFF.")
-	elif len(input) == 0:#If nothing is written, will repspond with text.
-		print("Nothing was written.")
+
+client.on_message = messageReceived
+
+while (True):
+	client.loop()
+####################################################################### Manual ON - OFF - through raw input
+
+# while True:
+	# input = raw_input("On or off ? ") #Input stored in input
+
+	# if input == "on":    #If the input == "on","ON",'1' will turn LED ON.
+	# 	port.write("1")
+	# 	print("LED ON.")
+	# elif input == "ON":
+	# 	port.write("1")
+	# 	print("LED ON.")
+	# elif input == "1":
+	# 	port.write("1")
+	# 	print("LED ON.")
+	# elif input == "off":#If the input == "off","OFF",'0' will turn LED OFF.
+	# 	port.write("0")
+	# 	print("LED OFF.")
+	# elif input == "OFF":
+	# 	port.write("0")
+	# 	print("LED OFF.")
+	# elif input == "0":
+	# 	port.write("0")
+	# 	print("LED OFF.")
+	# elif len(input) == 0:#If nothing is written, will repspond with text.
+	# 	print("Nothing was written.")
--- Chatbot/Chatbot.py
+++ Chatbot/Chatbot.py
@@ -1,5 +1,7 @@
+import urllib2
 
-learnWords = {}
+
+#learnWords = {}
 file = open("stop-words.txt")
 stopwords = file.readlines()
 #####################################################################################
@@ -11,16 +13,52 @@ def filterOutStopwords(filteringString): #function to filter out stop words: and
         filteringString = filteringString.replace(" " + next + " ", " ").lower()
     return filteringString
 #####################################################################################
+def wiki(wikiword):
+    gotUrl = urllib2.urlopen("http://en.wikipedia.org/wiki/"+wikiword) #pull in wiki search, then read the file.
+    #gotUrl = urllib2.urlopen("http://www.urbandictionary.com/define.php?term="+wikiword)
+    html = gotUrl.read()
+
+    startOfPTag = html.find("<p")
+    endOfPTag = html.find(">",startOfPTag)
+    endP = html.find("</p>", endOfPTag) #Find the p tag in html, both beggining and end, then store the inbetween in a p variable.
+    description = html[endOfPTag:endP]
+    return description #return everything between the p tags.
+
+#####################################################################################
 def feelings(): #Feelings function, analyses what your mood is.
     input = raw_input("How are you feeling?: ")
     filtered = filterOutStopwords(input)
     #if statement to see what the user is feeling.
     if filtered == 'sad':
-        print("Im sorry to hear that.")
+        print("Im sorry to hear that " + name)
+        chatbotMemory()
     elif filtered == 'happy':
         print("Im glad to hear! :)")
+        chatbotMemory()
     elif filtered == 'bored':
-        print("Thats why im here.")
+        print("Thats why im here. " + name)
+        chatbotMemory()
+    elif filtered == 'meh':
+        print("Well, meh is a valid response.")
+        chatbotMemory()
+    elif filtered == 'ok':
+        print("It's good to be ok! " + name)
+        chatbotMemory()
+    elif filtered == 'great':
+        print("Wow, I havn't talked to someone so positive in a while.")
+        chatbotMemory()
+    elif filtered == 'good':
+        print("That's good.")
+        chatbotMemory()
+    elif filtered == 'shit':
+        print("Must be bad if you'r swearing!")
+        chatbotMemory()
+    elif filtered == 'fucked':
+        print("Well that is a bad way to feel :[")
+        chatbotMemory()
+    elif filtered == 'nervous':
+        print("Why so nervous? " + name)
+        nervous()
     elif len(filtered) == 0:
         print("Come on, you can talk to me :)")
         feelings()
@@ -28,10 +66,36 @@ def feelings(): #Feelings function, analyses what your mood is.
         print("I dont know that emotion")
         feelings()
 #####################################################################################
+def nervous(): #nervous section, lets the user vent...
+    input = raw_input("Tell me what your nervous about?: ")
+    filtered = filterOutStopwords(input)
+    if len(filtered) > 0:
+        print("Tell me more...")
+        startwiki()
+    elif len(filtered) == 0:
+        print("you can talk to me :)")
+        nervous()
+
+def startwiki():
+    if len(filtered) > 0:
+        chatbotMemory()
+    elif len(filtered) == 0:
+        print("you can talk to me :)")
+#####################################################################################
 def chatbotMemory():
-    input = raw_input("Why do you feel this way? Explain with its name then Description: ")#chatbot asks why you feel this way, stores info in a dictionary.
-    for words in input:
-        mem = input.split().lower()
+    input = raw_input("With one word, describe why you feel like this: ")#chatbot asks why you feel this way, stores info in a dictionary.
+    for word in input:
+        filtered = filterOutStopwords(input)
+        filtered = filtered.replace("because " , " ")
+        filtered = filtered.replace(" because " , " ")
+        filtered = filtered.replace("well " , " ")
+        filtered = filtered.replace(" well " , " ")
+        description = wiki(filtered)
+        #description = description.replace(">" , " ")
+        #description = description.replace(">" , " ")
+        description = description.replace("<b>" , " ")
+        description = description.replace("</b>" , " ")
+    print("From your word: " + filtered + ". I found: " + description)
         
 #####################################################################################
 input = raw_input("Hi!, What is your name?: ")#meeting the chat bot
